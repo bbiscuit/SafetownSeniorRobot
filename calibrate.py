c9874267835ac7ee651016ddee5623e13d3a547e
Andrew Huffman <55068248+bbiscuit@users.noreply.github.com>
2023-11-27 12:05:06 -0500

    Primitive center detection (#2)
    * bad implementation, too slow
    
    * new approach, diagrammed in comments.
    
    * Pull camera device for VideoStreamer out into a constant value.
    
    * Added a gitignore
    
    * Improve data sharing across calibration and detection apps.
    
    * Made the sliders their own window in the calibration app.
    
    * Add click listener, which will be used to calibrate color.
    
    * Added the ability to calibrate line color in the calibration app
    
    * Now using color calibration, but not presently working. Done for the day.
    
    * Fix bug with color calibration: now done through the frame window.
    
    * Improved masking for tape.
    
    * appease pylint.
    
    * In a working state, but needs some improvements. Will do in future PR.

--- calibrate.py
+++ calibrate.py
@@ -1,111 +1,128 @@
+"""
+A standalone app to calibrate the camera for the SafeTown senior robot. This is not intended
+to be imported by other scripts.
+
+Author: Andrew Huffman
+"""
+
+import json
+import functools
 import cv2
 import numpy as np
-import csv
 
-topLeftCoord = [0, 0]
-topRightCoord = [0, 0]
-bottomLeftCoord = [0, 0]
-bottomRightCoord = [0, 0]
+SETTINGS_FILENAME = "settings.json"
+TRACKER_RADIUS = 10
+TRACKER_COLOR = (0, 0, 255)
 
-def loadCoordsFromCSV(filename):
-    try:
-        with open(filename, 'r') as csv_file:
-            reader = csv.reader(csv_file, dialect="excel")
-            for row in reader:
-                if len(row) >= 3:
-                    if row[0] == 'Top Left':
-                        topLeftCoord[0] = int(row[1])
-                        topLeftCoord[1] = int(row[2])
-                    elif row[0] == 'Top Right':
-                        topRightCoord[0] = int(row[1])
-                        topRightCoord[1] = int(row[2])
-                    elif row[0] == 'Bottom Left':
-                        bottomLeftCoord[0] = int(row[1])
-                        bottomLeftCoord[1] = int(row[2])
-                    elif row[0] == 'Bottom Right':
-                        bottomRightCoord[0] = int(row[1])
-                        bottomRightCoord[1] = int(row[2])
-    except FileNotFoundError:
-        print("Could not find previous calibration information.")
-
-def updateFrameWindow(frame, winName):
-    # Draw the trackers on the frame.
-    RAD = 10
-    COLOR = (0, 0, 255)
-    cv2.circle(frame, topLeftCoord, RAD, COLOR, -1)
-    cv2.circle(frame, topRightCoord, RAD, COLOR, -1)
-    cv2.circle(frame, bottomLeftCoord, RAD, COLOR, -1)
-    cv2.circle(frame, bottomRightCoord, RAD, COLOR, -1)
-
-    cv2.imshow(winName, frame)
-
-def warp(rows, cols, frame):
-    input_pts = np.float32([topLeftCoord, topRightCoord, bottomLeftCoord, bottomRightCoord])
+def warp(frame, coords):
+    """Copies the frame and warps it to be rectangular, with the coordinates as the corners."""
+    rows, cols, _ = frame.shape
+    input_pts = np.float32(
+        [coords["top_left"], coords["top_right"], coords["bottom_left"], coords["bottom_right"]]
+        )
     output_pts = np.float32([[0, 0], [cols, 0], [0, rows], [cols, rows]])
     transform = cv2.getPerspectiveTransform(input_pts, output_pts)
-    frame = cv2.warpPerspective(frame, transform, (cols, rows))
+    frame = cv2.warpPerspective(frame.copy(), transform, (cols, rows))
     return frame
 
-def writeCoordsToCSV(filename):
-    with open(filename, 'w') as csv_file:
-        writer = csv.writer(csv_file, dialect="excel")
-        writer.writerow(['Top Left', topLeftCoord[0], topLeftCoord[1]])
-        writer.writerow(['Top Right', topRightCoord[0], topRightCoord[1]])
-        writer.writerow(['Bottom Left', bottomLeftCoord[0], bottomLeftCoord[1]])
-        writer.writerow(['Bottom Right', bottomRightCoord[0], bottomRightCoord[1]])
+def draw_trackers(frame, coords):
+    """Draws the calibration trackers on the given frame (doesn't copy)."""
+    cv2.circle(frame, coords["top_left"], TRACKER_RADIUS, TRACKER_COLOR, -1)
+    cv2.circle(frame, coords["top_right"], TRACKER_RADIUS, TRACKER_COLOR, -1)
+    cv2.circle(frame, coords["bottom_left"], TRACKER_RADIUS, TRACKER_COLOR, -1)
+    cv2.circle(frame, coords["bottom_right"], TRACKER_RADIUS, TRACKER_COLOR, -1)
 
-# Load previous calibration information as a meaninful default.
-CSV_NAME = 'camWarpCalibration.csv'
-loadCoordsFromCSV(CSV_NAME)
+def create_tracker_window(frame, name: str, coords: dict):
+    """Creates a window which contains trackbars to configure warping. Takes in coordinate values
+    as default trackbar vals."""
+    frame_rows, frame_cols, _ = frame.shape
 
-# Set up camera information.
-cam = cv2.VideoCapture(0)
-FRAME_ROWS, FRAME_COLS, _ = cam.read()[1].shape
+    # This helper function exists to keep the size of this function down. This function was much,
+    # much longer before it existed.
+    def create_trackbar(text, coord, idx):
+        # Update functions. This has to be a fucntion and not a lambda because it's doing
+        # assignment.
+        def update_coordinate(val, coord, idx):
+            coords[coord][idx] = val
 
-# Initialize the window which will be used for calibration.
-def createCallibrationWindow(win_name):
-    cv2.namedWindow(win_name)
-    def updateTopLeftX(v):
-        topLeftCoord[0] = v
-    def updateTopLeftY(v):
-        topLeftCoord[1] = v
-    def updateTopRightX(v):
-        topRightCoord[0] = v
-    def updateTopRightY(v):
-        topRightCoord[1] = v
-    def updateBottomLeftX(v):
-        bottomLeftCoord[0] = v
-    def updateBottomLeftY(v):
-        bottomLeftCoord[1] = v
-    def updateBottomRightX(v):
-        bottomRightCoord[0] = v
-    def updateBottomRightY(v):
-        bottomRightCoord[1] = v
-    cv2.createTrackbar('Top Left X', win_name, topLeftCoord[0], FRAME_COLS, updateTopLeftX)
-    cv2.createTrackbar('Top Left Y', win_name, topLeftCoord[1], FRAME_ROWS, updateTopLeftY)
-    cv2.createTrackbar('Top Right X', win_name, topRightCoord[0], FRAME_COLS, updateTopRightX)
-    cv2.createTrackbar('Top Right Y', win_name, topRightCoord[1], FRAME_ROWS, updateTopRightY)
-    cv2.createTrackbar('Bottom Left X', win_name, bottomLeftCoord[0], FRAME_COLS, updateBottomLeftX)
-    cv2.createTrackbar('Bottom Left Y', win_name, bottomRightCoord[1], FRAME_ROWS, updateBottomLeftY)
-    cv2.createTrackbar('Bottom Right X', win_name, bottomRightCoord[0], FRAME_COLS, updateBottomRightX)
-    cv2.createTrackbar('Bottom Right Y', win_name, bottomRightCoord[1], FRAME_ROWS, updateBottomRightY)
+        cv2.createTrackbar(
+            text,
+            name,
+            coords[coord][idx],
+            frame_cols if idx == 0 else frame_rows,
+            functools.partial(update_coordinate, coord=coord, idx=idx)
+        )
 
-createCallibrationWindow('frame')
-i = 0
-while True:
-    ret, frame = cam.read()
-    warped = warp(FRAME_ROWS, FRAME_COLS, frame.copy())
+    cv2.namedWindow(name)
+    create_trackbar("Top Left X", "top_left", 0)
+    create_trackbar("Top Left Y", "top_left", 1)
+    create_trackbar("Top Right X", "top_right", 0)
+    create_trackbar("Top Right Y", "top_right", 1)
+    create_trackbar("Bottom Left X", "bottom_left", 0)
+    create_trackbar("Bottom Left Y", "bottom_left", 1)
+    create_trackbar("Bottom Right X", "bottom_right", 0)
+    create_trackbar("Bottom Right Y", "bottom_right", 1)
 
-    if not ret:
-        print("Can't get the frame!")
-        break
-    elif cv2.waitKey(1) == 13:
-        break
+def main():
+    """Driver code for the calibration app."""
+    # Load settings.
+    settings = {}
+    with open(SETTINGS_FILENAME, 'r', encoding='ascii') as settings_f:
+        settings = json.load(settings_f)
 
+    # Load coordinates from settings, as a meaninful default.
+    coords = settings["cam_warp_calibration"]
 
-    updateFrameWindow(frame, 'frame')
-    cv2.imshow('warped', warped)
-    i += 1
+    # Set up camera information.
+    cam = cv2.VideoCapture(settings["cam_device"])
 
-# Output a cam calibration CSV file.
-writeCoordsToCSV(CSV_NAME)
+    # Initialize the window which will be used for calibration.
+    create_tracker_window(cam.read()[1], "trackers", coords)
+
+    # Setup the mouse pointer, to calibrate tape colors.
+    cv2.namedWindow('calibrate')
+    color_calibration = [True, False]
+    def calibrate_color_with_mouse(event, x, y, _, __):
+        _, frame = cam.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        if event == cv2.EVENT_LBUTTONDOWN:
+            color_settings = settings["color_calibration_hsv"]
+            color = frame[y, x]
+
+            if color_calibration[0]:
+                color_settings["center_line"] = [int(color[0]), int(color[1]), int(color[2])]
+                print("Recorded center line color!")
+                color_calibration[0] = False
+                color_calibration[1] = True
+            elif color_calibration[1]:
+                color_settings["outside_line"] = [int(color[0]), int(color[1]), int(color[2])]
+                print("Recorded outside line color!")
+                color_calibration[1] = False
+
+    cv2.setMouseCallback('calibrate', calibrate_color_with_mouse)
+
+    i = 0
+    while True:
+        ret, frame = cam.read()
+        warped = warp(frame, coords)
+
+        # Handle exit conditions.
+        if not ret:
+            print("Can't get the frame!")
+            break
+        if cv2.waitKey(1) == 13:
+            break
+
+        # Update the frames being shown.
+        draw_trackers(frame, coords)
+        cv2.imshow('calibrate', frame)
+        cv2.imshow('warped', warped)
+        i += 1
+
+    # Write back the settings to file.
+    with open(SETTINGS_FILENAME, 'w', encoding='ascii') as settings_f:
+        json.dump(settings, settings_f)
+
+if __name__ == "__main__":
+    main()
